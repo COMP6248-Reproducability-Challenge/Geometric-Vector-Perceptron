b08203fd111f191f2edf030400f32542552cb368
kewyueshuen <kewyueshuen@gmail.com>
2021-05-08 06:59:02 +0100

    changed link to include ocr.npy

--- gvp/data/make_dataset.py
+++ gvp/data/make_dataset.py
@@ -6,7 +6,10 @@ def download_synthetic_dataset():
     # https://drive.google.com/drive/folders/1XuCyPFTM2Ro9kfCP_tWOF1uwBB-Hn3LH
 
     # url = "https://drive.google.com/drive/folders/1XuCyPFTM2Ro9kfCP_tWOF1uwBB-Hn3LH"
-    url = "https://drive.google.com/drive/folders/1ip1q3zKL2N3-pubdOBnk0dw9AnnQSNpE"
+    # url = "https://drive.google.com/drive/folders/1ip1q3zKL2N3-pubdOBnk0dw9AnnQSNpE"
+    
+    # OCR.npy included
+    url = "https://drive.google.com/drive/folders/1b4t-ZjyGOHLK-NJdOukFSdYVvZdWfv5m?usp=sharing"
 
     # mkdir if it doesnt exist
     data_path = Path("data/synthetic")
