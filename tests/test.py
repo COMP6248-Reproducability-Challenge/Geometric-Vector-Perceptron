45fbcecdc9905b6d64db603473e1455457945d7f
kewyueshuen <kewyueshuen@gmail.com>
2021-05-20 15:04:38 +0100

    cath download script and final test script

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,6 @@
-# from gvp.data.make_dataset import download_synthetic_dataset
-
-
-# download_synthetic_dataset()
+#=========================================================================================================#
+# Adapted slightly from https://github.com/lucidrains/geometric-vector-perceptron/blob/main/tests/tests.py
+#=========================================================================================================#
 
 import numpy as np
   
@@ -49,60 +48,6 @@ def test_equivariance_invariance():
     assert err_invar < TOL**2, 'invariance must be respected'
 
 
-def test_all_layer_types():
-    R = random_rotation()
-
-    model = GVP(
-        dim_vectors_in = 1024,
-        dim_feats_in = 512,
-        dim_vectors_out = 256,
-        dim_feats_out = 512
-    )
-    dropout = GVPDropout(0.2)
-    layer_norm = GVPLayerNorm(512)
-
-    feats = torch.randn(1, 512)
-    message = torch.randn(1, 512)
-    vectors = torch.randn(1, 32, 3)
-
-
-    # GVP layer
-    feats_out, vectors_out = model( (feats, diff_matrix(vectors)) )
-    assert list(feats_out.shape) == [1, 512] and list(vectors_out.shape) == [1, 256, 3]
-
-    # GVP Dropout
-    feats_out, vectors_out = dropout(feats_out, vectors_out)
-    assert list(feats_out.shape) == [1, 512] and list(vectors_out.shape) == [1, 256, 3]
-
-    # GVP Layer Norm
-    feats_out, vectors_out = layer_norm(feats_out, vectors_out)
-    assert list(feats_out.shape) == [1, 512] and list(vectors_out.shape) == [1, 256, 3]
-
-
-def test_mpnn():
-    # input data
-    x = torch.randn(5, 32)
-    edge_idx = torch.tensor([[0,2,3,4,1], [1,1,3,3,4]]).long()
-    edge_attr = torch.randn(5, 16)
-    # nodes (8 scalars and 8 vectors) || edges (4 scalars and 3 vectors)
-    dropout = 0.1
-    # define layer
-    gvp_mpnn = GVP_MPNN(feats_x_in = 8,
-                        vectors_x_in = 8,
-                        feats_x_out = 8,
-                        vectors_x_out = 8, 
-                        feats_edge_in = 4,
-                        vectors_edge_in = 4,
-                        feats_edge_out = 4,
-                        vectors_edge_out = 4,
-                        dropout=0.1 )
-    x_out    =  gvp_mpnn(x, edge_idx, edge_attr)
-
-    assert x.shape == x_out.shape, "Input and output shapes don't match"
-
-
 if __name__ == "__main__":
     test_equivariance_invariance()
-    test_all_layer_types()
-    test_mpnn()
     print('Completed tests')
